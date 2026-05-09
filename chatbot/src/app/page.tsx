b237026d7b7767d432dca35cf6719a1b7710bd52
Nimisha <nimishaagrawal115@gmail.com>
2026-05-09 22:24:13 +0530

    fix page

--- chatbot/src/app/page.tsx
+++ chatbot/src/app/page.tsx
@@ -1,9 +1,5 @@
-import ChatWidget from "../components/ChatWidget";
+import ChatWidget from "@/components/ChatWidget";
 
 export default function Home() {
-  return (
-    <>
-      <ChatWidget />
-    </>
-  );
+  return <ChatWidget />;
 }
